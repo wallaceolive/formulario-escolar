35ab36242d87aa29252b6d2bf08512053d9e5adb
wallaceolive <wallaceolive@gmail.com>
2026-06-08 20:00:04 -0300

    Formulario

---
+++ script.js
@@ -0,0 +1,62 @@
+function login() {
+
+    const email = document.getElementById('input-email').value
+    const senha = document.getElementById('input-senha').value
+
+
+    if (email === "" || senha === "") {
+        alert("Preencha todos os campos")
+        return
+    }
+
+    if (senha.length < 6) {
+        alert("Senha precisa ter no mínimo 6 caracteres")
+    }
+
+    if (email === "admin@email.com" && senha === "123456") {
+        window.location.href = 'formulario.html'
+        return
+    }
+    else {
+        alert("Credenciais inválidas")
+        return
+    }
+
+
+}
+
+
+const form = document.getElementById('myform')
+
+form.addEventListener('submit', event => {
+        event.preventDefault()
+       cadastrar()
+       
+})
+
+
+function cadastrar() {
+    const nome = document.getElementById('nome-aluno').value
+    const sobrenome = document.getElementById('sobrenome-aluno').value
+    const email = document.getElementById('email-aluno').value
+    const idade = document.getElementById('idade-aluno').value
+    const escolaridade = document.getElementById('escolaridade').value
+    const sexoSelecionado = document.querySelector("input[name='sexo']:checked")
+    const turnoSelecionado = document.querySelectorAll("input[name='turno']:checked")
+    
+    if(nome==="" || sobrenome==="" || email==="" || idade==="" || escolaridade==="") {
+        alert("Preencha todos os campos")
+        return
+    }
+
+    if(!sexoSelecionado) {
+        alert("Escolha um sexo")
+        return
+    }
+
+    if(turnoSelecionado.length===0) {
+        alert("Escolha um turno")
+        return
+    }
+
+}
